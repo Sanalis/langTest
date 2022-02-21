46db50cd8916547bdd1395f88cb2f072b7755790
Sanalis <sanalis@wp.pl>
2022-02-21 09:42:57 +0100

    Added first test

--- cypress/integration/lang-tests/ge_spec.js
+++ cypress/integration/lang-tests/ge_spec.js
@@ -1,15 +1,21 @@
+
+
 describe('German language', function () {
+//TODO move to fixture if possible 
+     Cypress.on('uncaught:exception', (err, runnable) => {
+       //TODO check that only the acceptable errors are skipped (__tfcapi etc.)
+        return false
+        });
+
     it('Clicks an element', function () {
         cy.visit('https://playsstar.com/worldwide/aisad-ob-ge')
 
-       // cy.pause()
+        cy.get('#menu-item-1819').should('have.text', 'Haftungsausschluss')
+        cy.get('#menu-item-1818').should('have.text', 'Datenschutzbestimmungen')
+        cy.get('#menu-item-1820').should('have.text', 'Werben sie mit uns')
+        cy.get('#menu-item-9785').should('have.text', 'Impressum')
         
-        //cy.contains('type').click()
-
-        //cy.url()
-      //  .should('include', '/commands/actions') //*[@id="menu-item-1818"]
-
-        cy.get('//*[@id="menu-item-1818"]')
-            .should('have.value', 'Datenschutzbestimmungen')
     })
 })
+
+
